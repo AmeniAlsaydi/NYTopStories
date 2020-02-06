308d6eb4d8c8f251cf9b58da61422224c7a21ef8
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-02-06 12:52:46 -0500

    Created api client for top stories

--- NYTopStories/Networks/TopStoriesApiClient.swift
+++ NYTopStories/Networks/TopStoriesApiClient.swift
@@ -9,4 +9,27 @@
 import Foundation
 import NetworkHelper
 
-
+struct NYTopStoriesApiClient {
+    static func fetchTopStories(for section: String, completeion: @escaping (Result<[Article], AppError>) -> ()){
+        let endpoint = "https://api.nytimes.com/svc/topstories/v2/nyregion.json?api-key=" // add key
+        guard let url = URL(string: endpoint) else {
+            completeion(.failure(.badURL(endpoint)))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let appError):
+                completeion(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do {
+                    let stories = try JSONDecoder().decode(TopStories.self, from: data)
+                    completeion(.success(stories.results))
+                } catch {
+                    completeion(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+}
