e36546b795f71bf4e0706b9f4fc1eede65701da0
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-02-06 10:49:34 -0500

    set up tab bar controller

--- NYTopStories/NewsController.swift
+++ NYTopStories/NewsController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  NewsController.swift
 //  NYTopStories
 //
 //  Created by Amy Alsaydi on 2/6/20.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class NewsController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    }
+    
 
 }
-
---
+++ NYTopStories/TopStoriesTabController.swift
@@ -0,0 +1,22 @@
+//
+//  TopStoriesTabController.swift
+//  NYTopStories
+//
+//  Created by Amy Alsaydi on 2/6/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import UIKit
+
+class TopStoriesTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = #colorLiteral(red: 0.949457705, green: 0.6923578978, blue: 0.7100731134, alpha: 1)
+        
+    }
+    
+
+
+}
